168fe84a8ec1f2f589217fcc3cdb7d851ff8ff60
rick pascua <rick.pascua@gmail.com>
2022-05-27 14:27:37 +0800

    working angularjs calling grails rest api

--- apps.js
+++ apps.js
@@ -1,44 +1,81 @@
-(function() {
-    
+(function () {
+
     var uId = 0;
 
     angular.module('studentApp', [])
-    .controller('StudentController', ['$scope', StudentController]);
+        .controller('StudentController', ['$scope', '$http', StudentController]);
 
-    function StudentController($scope) {
+    function StudentController($scope, $http) {
         $scope.newStudent = null;
-        $scope.students = [];
 
-        $scope.saveStudent = function() {
+        $http({
+            method: 'GET',
+            url: 'http://localhost:8080/api/student'
+        }).then(function success(response) {
+            $scope.students = response.data;
+        });
+
+        $scope.studentsList = function () {
+            $http({
+                method: 'GET',
+                url: 'http://localhost:8080/api/student'
+            }).then(function success(response) {
+                $scope.students = response.data;
+            });
+        }
+
+        $scope.saveStudent = function () {
+            console.log('saveStudent');
             if ($scope.newStudent.id == null) {
-                uId = uId + 1;
-                $scope.newStudent.id = uId;
-                $scope.students.push($scope.newStudent);
-            } else {
-                for (var i in $scope.students) {
-                    if ($scope.students[i].id == $scope.newStudent.id) {
-                        $scope.students[i] = $scope.newStudent;
+                $http.post('http://localhost:8080/api/student', $scope.newStudent).then(
+                    function successCallBack(response) {
+                        console.log('post success');
+                        $scope.students = $scope.studentsList();
+                    },
+                    function errorCallBack(response) {
+                        console.log('post error occured');
                     }
-                }
+                );
+            } else {
+                $http({
+                    method: 'PUT',
+                    url: 'http://localhost:8080/api/student/'+$scope.newStudent.id,
+                    data: JSON.stringify($scope.newStudent)
+                })
+                    .then(function (success) {
+                        console.log('success');
+                        $scope.students = $scope.studentsList();
+                    }, function (error) {
+                        console.log('errorCallback ' + error.data);
+                    });
+
             }
             $scope.newStudent = null;
         }
 
-        $scope.edit = function(id) {
+        $scope.edit = function (id) {
             for (var i in $scope.students) {
                 if ($scope.students[i].id == id) {
                     $scope.newStudent = angular.copy($scope.students[i])
                 }
-            }            
-        }
-
-        $scope.delete = function(id) {
-            for (var i in $scope.students) {
-                if ($scope.students[i].id == id) {
-                    $scope.students.splice(i,1);
-                    $scope.newStudent = {}
-                }
             }
         }
+
+        $scope.delete = function (id) {
+            
+            $http({
+                method: 'DELETE',
+                url: 'http://localhost:8080/api/student/'+id
+            })
+                .then(function (success) {
+                    console.log('success');
+                    $scope.students = $scope.studentsList();
+                }, function (error) {
+                    console.log('errorCallback ' + error.data);
+                });
+        }
+
     }
+
+
 })();
